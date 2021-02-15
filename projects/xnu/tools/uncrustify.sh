7f6f468f9cddca70a7d838e36c9c12674bfbec6b
Lenni <lennivh24@gmail.com>
2021-02-15 21:53:24 +0000

    return can only be called from within a function

--- projects/xnu/tools/uncrustify.sh
+++ projects/xnu/tools/uncrustify.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-return
+# This is a stub file that was at some point present in this whole mess. It got deleted some time along the way
+# but is still referenced in a Makefile, so I created a stub:
+
+exit
